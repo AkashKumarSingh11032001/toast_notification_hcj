85d361fb44f67619687117d221636d2e28a041fc
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-08-16 16:47:12 +0530

    app done

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-
 let toastBox = document.getElementById("toastBox");
-let SuccessMsg = '<i class="fa-solid fa-circle-check"></i>Successfully submitted'
-let ErrorMsg= '<i class="fa-solid fa-circle-xmark"></i>Fix the Error'
-let InvalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid Input, check again'
+let SuccessMsg =
+  '<i class="fa-solid fa-circle-check"></i>Successfully submitted';
+let ErrorMsg = '<i class="fa-solid fa-circle-xmark"></i>Fix the Error';
+let InvalidMsg =
+  '<i class="fa-solid fa-circle-exclamation"></i> Invalid Input, check again';
 
-function showToast(msg){
-    let toast = document.createElement("div");
-    toast.classList.add("toast");
-    toast.innerHTML = msg;
-    toastBox.appendChild(toast);
+function showToast(msg) {
+  let toast = document.createElement("div");
+  toast.classList.add("toast");
+  toast.innerHTML = msg;
+  toastBox.appendChild(toast);
 
-    if(msg.includes('Error')){
-        toast.classList.add('error');
-    }
-    if(msg.includes('Invalid')){
-        toast.classList.add("invalid") ;
-    }
-    setTimeout(()=>{
-        toast.remove();
-    },6000);
+  if (msg.includes("Error")) {
+    toast.classList.add("error");
+  }
+  if (msg.includes("Invalid")) {
+    toast.classList.add("invalid");
+  }
+  setTimeout(() => {
+    toast.remove();
+  }, 6000);
 }
